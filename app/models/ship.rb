38e2dcdc8e585cb503db0f4c5d04b73c80c98bbd
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-10-24 21:24:53 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(name, type, booty)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def 
+  
 end
