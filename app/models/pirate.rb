1828d97dae8615ea3615299e13adb504f8ea9aaf
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-10-24 21:21:27 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(name, weight, height)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  end
+
+  def self.all
+    PIRATES
+  end
+  
 end
